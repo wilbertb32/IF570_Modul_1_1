50808368717b20c688e53bcf9821885c1a018747
wilbertb32 <wilbertbernardi2004@gmail.com>
2025-02-04 20:32:55 +0700

    Starting a new Compose Project

---
+++ app/src/main/java/ac/id/umn/WilbertBernardi_69185_modul_1_1/MainActivity.kt
@@ -0,0 +1,47 @@
+package ac.id.umn.WilbertBernardi_69185_modul_1_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ac.id.umn.WilbertBernardi_69185_modul_1_1.ui.theme.IF570_modul_1_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IF570_modul_1_1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IF570_modul_1_1Theme {
+        Greeting("Android")
+    }
+}
